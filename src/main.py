df5e6ca0ef86cd151870fc5418a9f07c0713f586
sweetsoph <sophia.verardods@gmail.com>
2024-08-19 20:53:45 -0300

    Adicionando segundo carro

--- src/main.py
+++ src/main.py
@@ -1,5 +1,26 @@
 from frota import *
 
+def operar_carro(carro : Carro):
+    print('1- Ligar motor')
+    print('2- Desligar motor')
+    print('3- Acelerar')
+
+    op = 0
+    while op not in (1, 2, 3):
+        op = int(input("Digite as opcoes[1-3]: "))
+
+    if op == 1:
+        carro.ligar()
+    elif op == 2:
+        carro.desligar()
+    elif op == 3:
+        v = float(input("Informe a velocidade: "))
+        t = float(input("Informe o tempo: "))
+        carro.acelerar(v, t)
+
+    print('Infos atuais do carro')
+    print(carro)
+
 if __name__ == "__main__":
     print('Cadastre um carro')
     nm_modelo = input('Digite o modelo: ')
@@ -8,32 +29,29 @@ if __name__ == "__main__":
 
     kms = float(input('Digite com quantos Kms: '))
 
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, kms, motor = True)
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, kms, motor = False)
+
+    print('Cadastre um carro')
+    nm_modelo = input('Digite o modelo: ')
+    nm_marca = input('Digite a marca: ')
+    nm_cor = input('Digite a cor: ')
+
+    kms = float(input('Digite com quantos Kms: '))
+
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, kms, motor = False)
 
     '''
     Controlando o carro até ele atingir 10000 Km
     '''
-    while carro1.odometro < 10000:
+    while carro1.odometro < 600 and carro2.odometro < 600:
         try:
-            print('1- Ligar motor')
-            print('2- Desligar motor')
-            print('3- Acelerar')
-
             op = 0
-            while op not in (1,2,3):
-                op = int(input("Digite as opcoes[1-3]: "))
-
-            if op == 1:
-                carro1.ligar()
-            elif op == 2:
-                carro1.desligar()
-            elif op == 3:
-                v = float(input("Informe a velocidade: "))
-                t = float(input("Informe o tempo: "))
-                carro1.acelerar(v, t)
-
-            print('Infos atuais do carro')
-            print(carro1)
+            while op not in (1, 2):
+                op = int(input("escolha um carro para operar: "))
+            if(op == 1):
+                operar_carro(carro1)
+            else:
+                operar_carro(carro2)
         except Exception as e:
             print("Erro!")
             print(e)
